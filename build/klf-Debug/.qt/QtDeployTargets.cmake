e728546993830ea85f500dfdb10dc1fe3acb46fd
fram87 <emilien.gauze12@gmail.com>
2026-01-18 18:11:56 +0100

    Message décrivant ce que tu as modifié

---
+++ build/klf-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_GitCarPlAyQT_FILE C:/Users/AqmOpti/Documents/CarPLayQT/GitCarPlAyQT/build/klf-Debug/GitCarPlAyQT.exe)
+set(__QT_DEPLOY_TARGET_GitCarPlAyQT_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_GitCarPlAyQT_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Bluetoothd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
