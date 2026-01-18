4a22109457fe66e6316fc3fde4bb36cc919e7dec
fram87 <emilien.gauze12@gmail.com>
2026-01-18 19:19:05 +0100

    Mes modifications Qt : Bluetooth, WiFi et interface

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include "bluetooth.h"
 #include <QApplication>
+#include <QLoggingCategory>
+#include <QBluetoothLocalDevice>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QBluetoothLocalDevice localDevice;
+    qDebug() << "Bluetooth isValid =" << localDevice.isValid();
+    qDebug() << "Bluetooth hostMode =" << localDevice.hostMode();
     MainWindow w;
     w.show();
     Bluetooth bt;
