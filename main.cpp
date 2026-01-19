adede4fb998ccbd77500dba611ae2fbef1dbcc2e
fram87 <emilien.gauze12@gmail.com>
2026-01-19 14:30:38 +0100

    Message décrivant ce que tu as modifié

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include "mainwindow.h"
-#include "bluetooth.h"
 #include <QApplication>
 #include <QLoggingCategory>
-#include <QBluetoothLocalDevice>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QBluetoothLocalDevice localDevice;
-    qDebug() << "Bluetooth isValid =" << localDevice.isValid();
-    qDebug() << "Bluetooth hostMode =" << localDevice.hostMode();
     MainWindow w;
     w.show();
-    Bluetooth bt;
-    bt.startScan();
     return a.exec();
     //test
 }
